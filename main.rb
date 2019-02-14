ab22319edc091fcfa00bfaeaabcd4e48ebc6110e
Jake Bills <jakebills1@gmail.com>
2019-02-14 11:22:58 -0700

    added color to prompt in main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,26 @@
+require 'colorize'
 def puts_git(cmd)
     puts `git #{cmd} -h`
     menu
 end
 
 def menu 
-    puts '1: Enter git command'
-    puts '2: Exit'
+    puts 'MAIN MENU'.colorize(:cyan)
+    puts '1: Enter git command'.colorize(:cyan)
+    puts '2: Exit'.colorize(:cyan)
     choice = gets.to_i
     case choice
     when 1
-	puts 'Enter git command'
+	puts 'Enter git command'.colorize(:green)
 	puts_git(gets.strip)
 	menu
     when 2
-	puts 'Thanks for using the program"
+	puts 'Thanks for using the program'
 	sleep(3)
 	print `clear`
 	exit
     else
-	puts 'Invalid Choice'
+	puts 'Invalid Choice'.colorize(:red)
 	menu
     end
 end
